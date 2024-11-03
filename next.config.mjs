f7c5d2438da4cd4db921888fe67c94b769157eb7
adenuga adewumi <adenugaadewumi01@gmail.com>
2024-11-03 01:18:57 +0100

    remove unnecessary error page

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Disable static optimization for error pages
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  output: 'standalone',
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
